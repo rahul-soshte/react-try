8ce80b9b735dcc607781c87d6698e8b738377da0
root <rahul.soshte4771@gmail.com>
2024-05-12 10:58:20 +0000

    initial files

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Post from './components/Post';
+
+function App() {
+  return (
+    <div className="App">
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Post.js
@@ -0,0 +1,9 @@
+function Post() {
+    return (
+      <div className="Post">
+        <p>Hello I am Rahul Soshte</p>
+      </div>
+    );
+  }
+  
+  export default Post;
