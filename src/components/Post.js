b6ff67296979af16ab280e869c2c7fa67d015710
root <rahul.soshte4771@gmail.com>
2024-05-13 13:17:37 +0000

    js bugs are bad, gotta live with them

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,6 +1,12 @@
+const names = ["Suppandi", "Tantri the Mantri"]
+
 function Post() {
-    return (
+  
+  const chosenName = Math.random() > 0.5? names[0]: names[1];
+
+  return (
       <div className="Post">
+        <p>{chosenName}</p>
         <p>Hello I am Rahul Soshte</p>
       </div>
     );
