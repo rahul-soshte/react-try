2e8a3edb3693526c84795d7ab4c29974f7126664
root <rahul.soshte4771@gmail.com>
2024-05-16 15:57:30 +0000

    css styling

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,3 +1,5 @@
+import classes from './Post.module.css'
+
 const names = ["Suppandi", "Tantri the Mantri"]
 
 function Post(props) {
@@ -5,9 +7,9 @@ function Post(props) {
   const chosenName = Math.random() > 0.5? names[0]: names[1];
 
   return (
-      <div className="Post">
-        <p>{props.author}</p>
-        <p>{chosenName}</p>
+      <div className={classes.post}>
+        <p className={classes.author}>{props.author}</p>
+        <p className={classes.text}>{chosenName}</p>
       </div>
     );
   }
