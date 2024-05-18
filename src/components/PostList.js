1feb3f8263c4ea258e535812133c9e1d383d2f16
root <rahul.soshte4771@gmail.com>
2024-05-18 13:22:27 +0000

    basic form

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,13 +1,17 @@
 import Post from "./Post";
 import classes from "./PostList.module.css";
+import NewPost  from "./NewPost";
 
 function PostList(props) {
 
     return (
+      <>
+        <NewPost />
         <ul className={classes.posts}>
           <Post author = "tom brady"/>
           <Post author = "snoop dog"/>
         </ul>
+      </>
       );
 }
 
