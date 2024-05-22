8c2e7964598208b751663d6ffbfc98ea95cb71a3
root <rahul.soshte4771@gmail.com>
2024-05-22 09:41:41 +0000

    lfting state up

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,16 +1,14 @@
 import classes from './Post.module.css'
 
-const names = ["Suppandi", "Tantri the Mantri"]
 
 function Post(props) {
   
-  const chosenName = Math.random() > 0.5? names[0]: names[1];
 
   return (
-      <div className={classes.post}>
-        <p className={classes.author}>{props.author}</p>
-        <p className={classes.text}>{chosenName}</p>
-      </div>
+    <li className={classes.post}>
+      <p className={classes.author}>{props.author}</p>
+      <p className={classes.text}>{props.body}</p>
+    </li>
     );
   }
   
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,16 +1,26 @@
 import Post from "./Post";
 import classes from "./PostList.module.css";
 import NewPost  from "./NewPost";
-
+import { useState } from "react";
 function PostList(props) {
+  const [enteredBody, setEnteredBody] = useState('');
+  const [enteredAuthor, setEnteredAuthor] = useState('');
 
-    return (
+  function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+
+  function authorChangeHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
+  return (
       <>
-        <NewPost />
-        <ul className={classes.posts}>
-          <Post author = "tom brady"/>
-          <Post author = "snoop dog"/>
-        </ul>
+      <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler} />
+      <ul className={classes.posts}>
+        <Post author={enteredAuthor} body={enteredBody} />
+        <Post author = "snoop dog"/>
+      </ul>
       </>
       );
 }
