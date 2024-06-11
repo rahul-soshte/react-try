925abbe94b5f821c6dc2ba1b11e2d0596af78f68
root <rahul.soshte4771@gmail.com>
2024-06-11 08:32:38 +0000

    fetching

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -7,13 +7,15 @@ import classes from './PostList.module.css';
 
 function PostsList({ isPosting, onStopPosting }) {
   const [posts, setPosts] = useState([]);
-
+  const [isFetching, setIsFetching] = useState(false)
   useEffect(() => {
     async function fetchPosts() {
-
+      setIsFetching(true)
       const response = await fetch('http://localhost:8000/posts')
       const resData = await response.json();
       setPosts(resData.posts || []);
+      setIsFetching(false)
+
     }
 
     fetchPosts();
@@ -39,19 +41,26 @@ function PostsList({ isPosting, onStopPosting }) {
           <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
         </Modal>
       )}
-      {posts.length > 0 && (
+      {!isFetching && posts.length > 0 && (
         <ul className={classes.posts}>
           {posts.map((post) => (
             <Post key={post.body} author={post.author} body={post.body} />
           ))}
         </ul>
       )}
-      {posts.length === 0 && (
+      {!isFetching && posts.length === 0 && (
         <div style={{ textAlign: 'center', color: 'white' }}>
           <h2>There are no posts yet.</h2>
           <p>Start adding some!</p>
         </div>
       )}
+      {isFetching && (
+        <div style={{ textAlign: 'center', color: 'white' }}>
+          <p>Loading posts...</p>
+        </div>
+      )}
+
+      
     </>
   );
 }
