12a564b530c5ac0a53a07fb6f54ddac073546018
root <rahul.soshte4771@gmail.com>
2024-06-11 09:01:07 +0000

    using react router

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import NewPost from './components/NewPost';
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {path: '/', element: <App />},
+  {path: '/create-post', element: <NewPost />}
+]);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
